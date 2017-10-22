3cf50994cd68cd59b88a2f29f4acc0af8f746d3d
sgrams <sjg@fmdx.pl>
2017-10-23 00:58:41 +0200

    Added license

--- 6-heapsort/heapsort.c
+++ 6-heapsort/heapsort.c
@@ -1,6 +1,9 @@
 /*
  * AL2.4, Stanisław Juliusz Grams (251000 UG MFI), 20171022
- * Compile with command `gcc -o heapsort heapsort.c -Wall -pedantic -std=gnu99`
+ * 
+ * 1. USING
+ *
+ * Compile with command `gcc -o heapsort heapsort.c -Wextra -Wall -pedantic -std=gnu99`
  * Syntax `./heapsort [-l (for iterative heapify function!)] -i inputFile -o outputFile`
  *
  * Input file is required. If output file is not set, defaults will be forced (out.txt).
@@ -14,6 +17,21 @@
  *
  * Heapsort definition has to differ from the pseudocode
  * I didn't want to use the global variable and I wanted to pass the heapify type, too.
+ *
+ * 2. LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
 */
 
