437e63f159e90804578d60455a530bd6793edaa2
sgrams <sjg@fmdx.pl>
2018-03-07 15:18:48 +0100

    meh

--- 13-red_black_tree/1-tree.c
+++ 13-red_black_tree/1-tree.c
@@ -21,7 +21,7 @@
 #define defaultSyntax         "%s -i keys.txt [-d (display tree) "\
 "-p (print tree in-order) -r (remove elements) -o (output dot file)]\n"
 #define defaultErrorSyntax    "%s: \033[31mfatal error:\033[0m "
-#define defaultDotOutFilepath "output.in"
+#define defaultDotOutFilepath "tree.in"
 
 typedef struct node {
   gint val;
@@ -32,13 +32,13 @@ typedef struct node {
   struct node *root;
 } node_t;
 
-void tree_close       (node_t *tree);
-void tree_print       (node_t *tree, FILE *output);
-void tree_print_dot   (node_t *tree, FILE *output);
-void tree_print_reds  (node_t *tree, FILE *output);
-void tree_print_conn  (node_t *tree, FILE *output);
-void tree_print_node  (node_t *node);
-void tree_display     (node_t *tree, gint space);
+void tree_close         (node_t *tree);
+void tree_print         (node_t *tree, FILE *output);
+void tree_print_dot     (node_t *tree, FILE *output);
+void tree_print_reds    (node_t *tree, FILE *output);
+void tree_print_conn    (node_t *tree, FILE *output);
+void tree_print_node    (node_t *node);
+void tree_display       (node_t *tree, gint space);
 
 void tree_rotate_le   (node_t *tree, node_t *x);
 void tree_rotate_ri   (node_t *tree, node_t *x);
@@ -49,8 +49,11 @@ node_t *tree_insert_node  (node_t *tree, node_t *z);
 node_t *tree_minimum      (node_t *tree);
 node_t *tree_search       (node_t *tree, gint value);
 node_t *tree_transplant   (node_t *tree, node_t *u, node_t *v);
+node_t *tree_delete       (node_t *tree, node_t *x);
+node_t *tree_delete_fixup (node_t *x);
 
-gint tree_level   (node_t *tree);
+gint tree_level         (node_t *tree);
+gint tree_node_number   (node_t *node);
 
 gint main (gint argc, gchar *argv[])
 {
@@ -99,10 +102,7 @@ gint main (gint argc, gchar *argv[])
         remove_flag = TRUE;
         break;
       case 'o':
-        if (!optarg)
-          output_dot_filepath = g_strdup(defaultDotOutFilepath);
-        else
-          output_dot_filepath = g_strdup(optarg);
+        output_dot_filepath = g_strdup(defaultDotOutFilepath);
         output_dot_file = fopen((const gchar *) output_dot_filepath, "w");
         if(!output_dot_file) {
           fprintf(stderr, defaultErrorSyntax
@@ -152,7 +152,8 @@ gint main (gint argc, gchar *argv[])
     for (i=0; i<buf_num; i++)
     {
       scanf("%i", &buf_rm);
-      //tree = tree_delete(tree, buf_rm);
+      if (tree_search(tree, buf_rm))
+        tree_delete(tree, tree_search(tree, buf_rm));
     }
   }
   
@@ -169,7 +170,8 @@ gint main (gint argc, gchar *argv[])
   }
 
   if (output_dot_file) {
-    tree_print_dot(tree, output_dot_file);
+    tree_print_dot(tree->root, output_dot_file);
+
   }
   
   /*
@@ -180,7 +182,6 @@ gint main (gint argc, gchar *argv[])
   g_free(input_filepath);
   g_free(output_dot_filepath);
 
-  fclose(input_file);
   return 0;
 }
 
@@ -204,6 +205,7 @@ void tree_print (node_t *tree, FILE *output) {
 }
 
 void tree_print_dot (node_t *tree, FILE *output) {
+  gchar* cmd[] = { "dot", "-Tsvg", "-otree.svg", "tree.in", NULL };
   fprintf(output,
       "digraph G {\n"
       "  graph [ratio=.36];\n"
@@ -214,6 +216,16 @@ void tree_print_dot (node_t *tree, FILE *output) {
 
   tree_print_conn(tree, output);
   fprintf(output, "}\n");
+  fclose(output);
+  if (!g_spawn_sync(NULL, cmd, NULL, G_SPAWN_SEARCH_PATH, 0, NULL, NULL, NULL, NULL, NULL)) {
+    fprintf(stderr, "Unable to open dot program.\n");
+  }
+  else {
+    gchar* cmd2[] = { "display", "tree.svg", NULL };
+    if (!g_spawn_async(NULL, cmd2, NULL, G_SPAWN_SEARCH_PATH, 0, NULL, NULL, NULL)) {
+      fprintf(stderr, "Unable to start display\n");
+    }
+  }
 }
 
 void tree_print_reds (node_t *tree, FILE *output) {
@@ -234,25 +246,30 @@ void tree_print_conn (node_t *tree, FILE *output) {
   static gint null_counter = 0;
   if (tree) {
     if (tree->le) {
-      fprintf(output, "  %i -> %i [weight=%i];\n", tree->val, tree->le->val, tree_level(tree));
+        fprintf(output, "  %i -> %i [weight=%i];\n", tree_node_number(tree), tree_node_number(tree->le), tree_level(tree));
       if (tree->le->colour == red)
-        fprintf(output, "%i [fillcolor=\"red\", color=\"red\"];\n", tree->le->val);
+        fprintf(output, "%i [fillcolor=\"red\", color=\"red\"];\n", tree_node_number(tree->le));
+      fprintf(output, "  %i [label=\"%i\"]\n", tree_node_number(tree), tree->val);
+
     }
     else {
       fprintf(output, "  %i -> null%i [weight=%i];\n  null%i [shape=box, label=\"nil\", fontsize=12, width=0.5, height=0.3];\n",
-          tree->val, null_counter, tree_level(tree), null_counter);
+          tree_node_number(tree), null_counter, tree_level(tree), null_counter);
+      fprintf(output, "  %i [label=\"%i\"]\n", tree_node_number(tree), tree->val);
       null_counter++;
     }
     tree_print_conn (tree->le, output);
     if (tree->ri)
     {
-      fprintf(output, "  %i -> %i [weight=%i];\n", tree->val, tree->ri->val, tree_level(tree));
+      fprintf(output, "  %i -> %i [weight=%i];\n", tree_node_number(tree), tree_node_number(tree->ri), tree_level(tree));
       if (tree->ri->colour == red)
-        fprintf(output, "%i [fillcolor=\"red\", color=\"red\"];\n", tree->ri->val);
+        fprintf(output, "%i [fillcolor=\"red\", color=\"red\"];\n", tree_node_number(tree->ri));
+      fprintf(output, "  %i [label=\"%i\"]\n", tree_node_number(tree), tree->val);
     }
     else {
       fprintf(output, "  %i -> null%i [weight=%i];\n  null%i [shape=box, label=\"nil\", fontsize=12, width=0.5, height=0.3];\n",
-          tree->val, null_counter, tree_level(tree), null_counter);
+          tree_node_number(tree), null_counter, tree_level(tree), null_counter);
+      fprintf(output, "  %i [label=\"%i\"]\n", tree_node_number(tree), tree->val);
       null_counter++;
     }
     tree_print_conn (tree->ri, output);
@@ -263,6 +280,15 @@ void tree_print_node (node_t *node) {
     printf("%i\n", node->val);
 }
 
+gint tree_node_number (node_t *node) {
+  if (node->pa == NULL)
+    return 0;
+
+  if (node == node->pa->le)
+    return tree_node_number (node->pa) * 2 + 1;
+  else
+    return tree_node_number (node->pa) * 2 + 2;
+}
 void tree_display (node_t *tree, gint space) {
   if (tree) {
     tree_display(tree->ri, space+4);
@@ -416,10 +442,134 @@ node_t *tree_insert (node_t *tree, gint value) {
   tree->root->colour = black;
   return tree;
 }
+node_t *tree_transplant (node_t *tree, node_t *u, node_t *v) {
+  node_t *p = u->pa;
+
+  if (!p)
+    tree->root = v;
+  else if (u == p->le)
+    p->le = v;
+  else
+    p->ri = v;
+
+  if (v)
+    v->pa = p;
+
+  return tree;
+}
+
+node_t *tree_delete  (node_t *tree, node_t *z) {
+  node_t *y = z;
+  node_t *x = NULL;
+  gint y_org_col = 0;
+
+  if (y->colour == red)
+    y_org_col = 1;
+
+  if (!z->le) {
+    x = z->ri;
+    tree_transplant(tree, z, z->ri);
+  }
+  else if (!z->ri) {
+    x = z->le;
+    tree_transplant(tree, z, z->le);
+  }
+  else {
+    y_org_col = y->colour;
+    x = y->ri;
+    if (y->pa == z) {
+      x->pa = y;
+    }
+    else {
+      tree_transplant(tree, y, y->ri);
+      y->ri = z->ri;
+      y->ri->pa = y;
+    }
+    tree_transplant(tree, z, y);
+    y->le = z->le;
+    y->le->pa = y;
+    y->colour = z->colour;
+  }
+
+  if (!y_org_col)
+    tree_delete_fixup(z);
+
+  return tree;
+}
+
+node_t *tree_delete_fixup (node_t *x) {
+  node_t *t = x->root;
+  node_t *w = NULL;
+  while (x != t && (x->colour == black))
+  {
+    if (x->pa && x == x->pa->le) {
+      w = x->pa->ri;
+      if (w && w->colour == red) {
+        // case 1
+        w->colour = black;
+        x->pa->colour = red;
+        tree_rotate_le(t, x->pa);
+        w = x->pa->ri;
+      }
+      if (w && w->le->colour == black && w->ri->colour == black) {
+        // case 2
+        w->colour = red;
+        x = x->pa;
+      }
+      else {
+        if (w && w->ri->colour == black) {
+          // case 3
+          w->le->colour = black;
+          w->colour = red;
+          tree_rotate_ri(t, w);
+          w = x->pa->ri;
+        }
+        // case 4
+        w->colour = x->pa->colour;
+        x->pa->colour = black;
+        w->ri->colour = black;
+        tree_rotate_le(t, x->pa);
+        x = t;
+      }
+    }
+    else {
+      w = x->pa->le;
+      if (w && w->colour == red) {
+        // case 1
+        w->colour = black;
+        x->pa->colour = red;
+        tree_rotate_ri(t, x->pa);
+        w = x->pa->le;
+      }
+      if (w && w->ri->colour == black && w->le->colour == black) {
+        // case 2
+        w->colour = red;
+        x = x->pa;
+      }
+      else {
+        if (w && w->le->colour == black) {
+          // case 3
+          w->ri->colour = black;
+          w->colour = red;
+          tree_rotate_le(t, w);
+          w = x->pa->le;
+        }
+        // case 4
+        w->colour = x->pa->colour;
+        x->pa->colour = black;
+        w->le->colour = black;
+        tree_rotate_ri(t, x->pa);
+        x = t;
+      }
+    }
+  }
+  x->colour = black;
+  return t;
+}
 
 node_t *tree_minimum (node_t *tree) {
   node_t *x = tree;
-  while (x->le)
+  while (x && x->le)
     x = x->le;
   return x;
 }
@@ -434,20 +584,6 @@ node_t *tree_search (node_t *tree, gint value) {
       root = root->ri;
   }
   return root;
-}
-
-node_t *tree_transplant (node_t *tree, node_t *u, node_t *v) {
-  if (!u->pa)
-    tree = v;
-  else if (u == u->pa->le)
-    u->pa->le = v;
-  else
-    u->pa->ri = v;
-  
-  if (v)
-    v->pa = u->pa;
-
-  return tree;
 }
 
 gint tree_level (node_t *tree) {
